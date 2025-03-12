b484121b07bbe0e57d2c1aaf7364c5e65dabcb29
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-03-13 00:36:10 +0530

    Made a chatbot with the help of ollama

--- package.py
+++ package.py
@@ -3,9 +3,15 @@ import ollama
 client = ollama.Client()
 
 model = "llama2"
-prompt = input("Enter your prompt here: ")
 
-response = client.generate(model=model, prompt=prompt)
+while(True):
+  
+  prompt = input("Enter your prompt here: ")
 
-print("Response from Ollama:")
-print(response.response)
+  if prompt == 'bye' or prompt == 'exit':
+    break
+  else:
+    response = client.generate(model=model, prompt=prompt)
+
+    print("Response from Ollama:")
+    print(response.response)
